8ba4b29a32f8f5eb42a6baf4d7e90bed68bc3a71
zhang <zhangtuo2008@gmail.com>
2016-08-17 11:18:07 +0200

    reduce

---
+++ reduce.js
@@ -0,0 +1,23 @@
+/**
+ * Created by tuo on 16/8/17.
+ */
+var http = require('http')
+    , async = require('async');
+
+async.reduce(['one', 'two', 'three'], 0, function(memo, item, done){
+    var body = '';
+
+    http.get(process.argv[2] + "?number=" + item, function(res){
+        res.on('data', function(chunk){
+            body += chunk.toString();
+        });
+
+        res.on('end', function(){
+            done(null, memo + Number(body));
+        });
+    }).on('error', done);
+
+}, function done(err, result){
+    if (err) return console.log(err);
+    console.log(result);
+});
